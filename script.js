bc67105b1e792364f1b534f068deb1c1f2339aa0
mhmd <hkr64578@gmail.com>
2025-01-11 11:39:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const video = document.getElementById('camera');
 
 // فعال کردن دوربین
 function startCamera() {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error("خطا در دسترسی به دوربین:", error);
-      alert("متأسفانه نمی‌توان به دوربین دسترسی پیدا کرد!");
-    });
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: { exact: "environment" } // فعال کردن دوربین پشتی
+    }
+  })
+  .then((stream) => {
+    video.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error("خطا در دسترسی به دوربین:", error);
+    alert("متأسفانه نمی‌توان به دوربین پشتی دسترسی پیدا کرد! شاید دوربین پشتی در دسترس نباشد.");
+  });
 }
 
 // اجرای دوربین به‌صورت پیش‌فرض
